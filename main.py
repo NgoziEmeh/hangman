bd3c4aa879f48835f499ca534703e61db26e6920
NgoziEmeh <zingo1ng@yahoo.com>
2021-11-07 03:55:27 -0800

    chosen word

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 #Make list of words
 
 #player picks one randomly from the list
@@ -17,6 +18,8 @@
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+print(chosen_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
