89d6616854f76e0554256730fb492989b75c5fa6
NgoziEmeh <zingo1ng@yahoo.com>
2021-11-07 04:02:38 -0800

    check letter

--- main.py
+++ main.py
@@ -22,5 +22,10 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
+guess = input("Guess a letter:").lower()
+print(guess)
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+if guess in chosen_word:
+    print("it is there")
+else:
+    print("it is not there")
