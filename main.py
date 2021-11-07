2a9a595a34f4c62b15aebd3de88938b08b0d15aa
NgoziEmeh <zingo1ng@yahoo.com>
2021-11-07 03:47:11 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Make list of words
+
+#player picks one randomly from the list
+
+#based on the length of the word, make dashes
+
+#player2 gueses one letter from the word,
+# if the letter is in the word,
+
+# 1 . insert the letter in its place in the word
+#if the letter is not in the word, add an ascia to the hang robe
+
+#enclose in a while loop so :
+#1. hangs and ends if the handman is fully drawn
+#2. end and player 2 wins if the word completes
