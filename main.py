b10ffbb186834d62ec54f10e6e8a7dc0f8f2c245
NgoziEmeh <zingo1ng@yahoo.com>
2021-11-07 03:52:38 -0800

    words in a list

--- main.py
+++ main.py
@@ -13,3 +13,11 @@
 #enclose in a while loop so :
 #1. hangs and ends if the handman is fully drawn
 #2. end and player 2 wins if the word completes
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
